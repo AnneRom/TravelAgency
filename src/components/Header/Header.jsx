99266daeae73a4e65cb4f383b538740d28df9fc8
AnneRom <coolann000.com@gmail.com>
2025-05-22 18:35:32 +0300

    Lesson_22-05
    Створили компонент Button, додали стилі для нього та для хедера.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,22 @@
-
+import styles from './Header.module.css'
+import Button from '../Button'
 import Logo from './Logo'
 import NavMenu from './NavMenu'
 
 const Header = () => {
     return (
         <header>
-            <Logo />
-            <NavMenu variant='white'/>
-            {/* <NavMenu variant='black'/> */}
-          
+            <div className={styles.header__container}>
+                <Logo />
+                <NavMenu variant='white'/>
+                {/* <NavMenu variant='black'/> */}
+                <div className={styles.headerRight}>
+                    <Button text="Login" widthBtn={77} variant="login"/>
+                    <Button text="Sign Up" widthBtn={168}/>
+                </div> 
+            
+            </div>
+            
         </header>
     )
 }
