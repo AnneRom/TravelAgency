3c7aa6e899823188b7c6a0d185445a67c9d70b66
AnneRom <coolann000.com@gmail.com>
2025-07-10 12:05:50 +0300

    Lessom_10-07
    Почали робити компонент LoginForm.jsx

--- src/components/Header/LoginForm.jsx
+++ src/components/Header/LoginForm.jsx
@@ -1,6 +1,43 @@
+import styles from './LoginForm.module.css'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { useId } from 'react'
+import * as Yup from 'yup'
+
+const FeedbackSchema = Yup.object().shape({
+    email: Yup.string().email("Must be a valid email!").required("Required"),
+    password: Yup.string().min(6, "Too short").max(256, "Too long").required("Required"),
+})
+
+const initialValues = {
+    email: "",
+    password: ""
+}
+
 const LoginForm = () => {
+    const handleSubmit = (values, actions) => {
+        console.log(values)
+        actions.resetForm()
+    }
+
+    const emailFieldId = useId()
+    const passwordFieldId = useId()
+
     return (
-        <h1>Login Form Title</h1>
+        <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={FeedbackSchema}>
+        <Form className={styles.form}>
+            <label htmlFor={emailFieldId}>Email Address</label>
+            <Field id={emailFieldId} type="email" name="email" className={styles.field}></Field>
+            <ErrorMessage name="email" component="span" className={styles.error}/>
+
+            <label htmlFor={passwordFieldId}>Email Address</label>
+            <Field id={passwordFieldId} type="password" name="password" className={styles.field}></Field>
+            <ErrorMessage name="password" component="span" className={styles.error}/>
+           
+
+            <button type="submit" className={styles.btn}>Submit</button>
+        </Form>
+      </Formik>
+        
     )
 }
 export default LoginForm
