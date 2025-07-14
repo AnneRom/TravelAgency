0e1db9442ef001d898b344386012394def9463d4
AnneRom <coolann000.com@gmail.com>
2025-07-14 12:03:22 +0300

    Lesson_14-07
    Modal.jsx додали handleBackdropClick (). Додали кнопку Eye, яка ховає/показую пароль.

--- src/components/Header/Modal.jsx
+++ src/components/Header/Modal.jsx
@@ -2,9 +2,15 @@ import styles from './Modal.module.css'
 import { CgClose } from 'react-icons/cg'
 
 const Modal = ({ onClose, title, children }) => {
+    const handleBackdropClick = (e) => {
+        if (e.target === e.currentTarget) {
+            onClose()
+        }
+    }
+    
     return (
-        <div className={styles.backdrop} onClick={onClose}>
-            <div className={styles.modal} onClick={(e) => e.stopPropagation}> 
+        <div className={styles.backdrop} onClick={handleBackdropClick}>
+            <div className={styles.modal}> 
                 <h2 className={styles.title}>{title}</h2>
                 <button className={styles.closeBtn} onClick={onClose}><CgClose size={12} color='black'/></button>  
                 {children}
